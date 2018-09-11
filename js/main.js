ce5410f07d59e1c74e60089126c40e14bb5ecea6
oephi <r.sickle@protonmail.com>
2018-09-11 16:55:26 +1000

    index styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
-    $("#image-fade_onclick").click(function() {
-    $("#image-fade img.top").toggleClass("transparent");
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
   });
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
   });
